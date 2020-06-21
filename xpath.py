7d3a101c54ccfc3da64d500eb33e75cc6ebd2b27
Artem <artemkurchakov1018@gmail.com>
2020-06-21 14:15:44 +0300

    locators

---
+++ xpath.py
@@ -0,0 +1,10 @@
+locators = {
+    "explore_demo": '//div[@class="container"]//a[@href="https://phptravels.com/demo/"]',
+    "front_end": '//div[@class="row"]//a[@href="//www.phptravels.net"]',
+    "back_end": '//div[@class="row"]//a[@href="//www.phptravels.net/admin"]',
+    "demo": '//div[@class="yx-njp"]//a[@href="https://phptravels.com/demo"]',
+    "pricing": '//div[@class="yx-njp"]//a[@href="https://phptravels.com/order"]',
+    "product": '//button[contains(text(), "Product")]',
+    "hosting": '//button[contains(text(), "Hosting")]',
+    "support": '//button[contains(text(), "Support")]',
+    "links": '//button[contains(text(), "Links")]'}
